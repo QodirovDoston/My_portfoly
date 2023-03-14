6df9406811eb908d58028cacc4aa06601d285347
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-14 19:39:38 +0500

    ddf

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import FooterD from "./FooterD/FooterD";
+// import HeaderD from "./HeaderD/HeaderD";
+import NavbarpageD from "./NavbarPageD/NavbarpageD";
+// import NavD from "./NavD/NavD";
+import React from 'react'
+function App() {
+  return (
+    <>
+    <div>
+      <NavbarpageD />
+      {/* <HeaderD />
+      <NavD />
+      <FooterD /> */}
+    </div>
+    </>
+  );
+}
+
+export default App;
